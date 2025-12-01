445b09dbf316d46a78d55101d617b09abc103f16
Denis <korbakovd@gmail.com>
2025-12-01 17:54:46 +0300

    feat: add more logic

--- cmd/go-http-template/main.go
+++ cmd/go-http-template/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
-	"github.com/jmoiron/sqlx"
-	"log"
-
-	"go-rest/internal/config"
+	"context"
 	"go.uber.org/zap"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
+	"go-rest/internal/config"
 )
 
 func main() {
@@ -14,6 +21,7 @@ func main() {
 	}
 }
 
+// run todo divide everything into layers
 func run() error {
 	logger, _ := zap.NewProduction()
 
@@ -23,7 +31,7 @@ func run() error {
 	logger.Info("starting app")
 	logger.Info("initializing config")
 
-	cfg, err := config.MustLoad()
+	cfg, err := config.Load()
 	if err != nil {
 		logger.Panic("cant init config, err: ", zap.Error(err))
 	}
@@ -34,12 +42,58 @@ func run() error {
 	if err != nil {
 		logger.Panic("cant init db, err: ", zap.Error(err))
 	}
-	defer func() {
-		err = conn.Close()
-		if err != nil {
-			logger.Error("cant close db, err: ", zap.Error(err))
+
+	if err = conn.Ping(); err != nil {
+		logger.Panic("cant connect to db, err: ", zap.Error(err))
+	}
+
+	logger.Info("initializing router")
+
+	router := mux.NewRouter()
+	//todo add route
+	//router.HandleFunc("/", HomeHandler)
+
+	logger.Info("initializing server")
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	srv := &http.Server{
+		Addr:         net.JoinHostPort("", cfg.ApiPort),
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPTimeout,
+		WriteTimeout: cfg.HTTPTimeout,
+		IdleTimeout:  cfg.IdleTimeout,
+	}
+
+	go func() {
+		logger.Info("starting server", zap.String("port", cfg.ApiPort))
+
+		if err = srv.ListenAndServe(); err != nil {
+			logger.Error("failed to start server, err: ", zap.Error(err))
 		}
 	}()
+
+	logger.Info("server started successfully")
+
+	<-done
+	logger.Info("stopping server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), cfg.ShutdownTimeout)
+	defer cancel()
+
+	logger.Info("shutting down server", zap.Duration("timeout", cfg.ShutdownTimeout))
+
+	if err = srv.Shutdown(ctx); err != nil {
+		logger.Error("Server forced to shutdown", zap.Error(err))
+	}
+
+	logger.Info("closing database connection")
+	if err := conn.Close(); err != nil {
+		logger.Error("failed to close db connection", zap.Error(err))
+	}
+
+	logger.Info("server stopped")
 
 	return nil
 }
