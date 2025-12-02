b275aa221257895ccbb68b04e4dd1819ce1300fe
Denis <korbakovd@gmail.com>
2025-12-02 15:52:19 +0300

    feat: create table users

---
+++ db/migration/001.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users
+(
+    id         bigserial PRIMARY KEY,
+    username   varchar(25) NOT NULL UNIQUE,
+    password   varchar(30) NOT NULL,
+    created_at timestamp,
+    updated_at timestamp
+);
